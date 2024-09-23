1a40d70e2fe6d294df172b9ae15a8dcdfa218743
avadlamani-tech <ashwinvadlamani4@gmail.com>
2024-09-23 15:33:46 -0700

    First mini Project

---
+++ app/src/main/java/com/example/android_mini_compose_projects/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.android_mini_compose_projects
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.android_mini_compose_projects.ui.theme.Android_Mini_Compose_ProjectsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+        }
+    }
+}
+
